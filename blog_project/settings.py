b85fd4472875fcbfc230686de9bc615959b0ff54
Katarzyna Płoskonka <kasplosk@gmail.com>
2022-02-12 10:16:10 +0100

    Reroll secret key for production usage

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n*)m%zhs75gvh^nw9$i1n70mh!t-^d5fla!dvwg6ytu7((hh10'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
